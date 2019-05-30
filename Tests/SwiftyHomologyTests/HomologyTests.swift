9f9149b651ccb09223541dbd9d75610521649f8b
Taketo Sano <taketo1024@gmail.com>
2019-05-30 17:00:54 +0900

    add test.

--- Tests/SwiftyHomologyTests/HomologyTests.swift
+++ Tests/SwiftyHomologyTests/HomologyTests.swift
@@ -6,8 +6,19 @@
 //
 
 import XCTest
+import SwiftyMath
+@testable import SwiftyHomology
+
+extension Matrix {
+    func asDMatrix() -> DMatrix<R> {
+        return DMatrix(rows: rows, cols: cols, grid: grid)
+    }
+}
 
 class HomologyTests: XCTestCase {
+    
+    typealias R = 𝐙
+    typealias A = AbstractBasisElement
 
     override func setUp() {
         super.setUp()
@@ -19,11 +30,57 @@ class HomologyTests: XCTestCase {
         super.tearDown()
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testHomology() {
+        let n = 3
+        let base = ModuleGrid1<A, R>(generators: Dictionary(pairs: (0 ... n).map{ i in
+            (i, A.generateBasis(1))
+        }))
+        let d = ChainMap(degree: -1) { (i) -> FreeModuleHom<A, A, R> in
+            FreeModuleHom(from: base[i]!.rootBasis, to: base[i - 1]!.rootBasis, matrix: Matrix1(0).asDMatrix())
+        }
+        let C = ChainComplex(base: base, differential: d)
+        let H = C.homology()
+        
+        XCTAssertEqual(H[0]!.structure, [0: 1])
+        XCTAssertEqual(H[1]!.structure, [0: 1])
+        XCTAssertEqual(H[2]!.structure, [0: 1])
+        XCTAssertEqual(H[3]!.structure, [0: 1])
     }
 
+    func testHomology2() {
+        let n = 3
+        let base = ModuleGrid1<A, R>(generators: Dictionary(pairs: (0 ... n).map{ i in
+            (i, A.generateBasis(1))
+        }))
+        let d = ChainMap(degree: -1) { (i) -> FreeModuleHom<A, A, R> in
+            FreeModuleHom(from: base[i]!.rootBasis, to: base[i - 1]!.rootBasis, matrix: Matrix1(i % 2 == 0 ? 0 : 1).asDMatrix())
+        }
+        let C = ChainComplex(base: base, differential: d)
+        let H = C.homology()
+        
+        XCTAssertEqual(H[0]!.structure, [:])
+        XCTAssertEqual(H[1]!.structure, [:])
+        XCTAssertEqual(H[2]!.structure, [:])
+        XCTAssertEqual(H[3]!.structure, [:])
+    }
+    
+    func testHomology3() {
+        let n = 3
+        let base = ModuleGrid1<A, R>(generators: Dictionary(pairs: (0 ... n).map{ i in
+            (i, A.generateBasis(1))
+        }))
+        let d = ChainMap(degree: -1) { (i) -> FreeModuleHom<A, A, R> in
+            FreeModuleHom(from: base[i]!.rootBasis, to: base[i - 1]!.rootBasis, matrix: Matrix1(i % 2 == 0 ? 0 : 2).asDMatrix())
+        }
+        let C = ChainComplex(base: base, differential: d)
+        let H = C.homology()
+        
+        XCTAssertEqual(H[0]!.structure, [2: 1])
+        XCTAssertEqual(H[1]!.structure, [:])
+        XCTAssertEqual(H[2]!.structure, [2: 1])
+        XCTAssertEqual(H[3]!.structure, [:])
+    }
+    
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measure {
